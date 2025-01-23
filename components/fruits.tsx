81834b776a88a9a9c9455dc7202bbf297d3f13d3
JApitpit <Jianpitpit@gmail.com>
2025-01-22 22:50:07 -0700

    Added fruits list

---
+++ components/fruits.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function Fruits() {
+    return (
+        <View>
+            <Text>Apple</Text>
+            <Text>Orange</Text>
+            <Text>Mango</Text>
+        </View>
+    )
+}
